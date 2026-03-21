265ed5caf9771d0820af0d4b59cacb84c6d5ca2d
Tyler Chin <tylerstyle05@gmail.com>
2026-03-21 00:08:52 -0600

    player bouncing mechanic

--- EarthsNewestSuperheroJam/Assets/Scripts/PlayerController.cs
+++ EarthsNewestSuperheroJam/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Movement")]
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float idleGravity = 5f;
+    [SerializeField] private PhysicsMaterial2D idleMaterial;
+    [SerializeField] private float poundGravity = 5f;
+    [SerializeField] private PhysicsMaterial2D poundMaterial;
+    [Header("Ground Detection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
     [Header("Input Actions")]
     [SerializeField] InputAction moveAction;
     [SerializeField] InputAction poundAction;
 
+    bool pounding = false;
     Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,7 +32,6 @@ public class PlayerController : MonoBehaviour
         moveAction.Enable();
         poundAction.Enable();
     }
-
     private void OnDisable()
     {
         moveAction.Disable();
@@ -35,12 +42,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        if (poundAction.ReadValue<float>() > 0.5f)
+        {
+            if (rb != null) rb.gravityScale = poundGravity;
+            if (rb != null) rb.sharedMaterial = poundMaterial;
+            pounding = true;
+        }
+        else
+        {
+            if (pounding && IsGrounded()) rb.AddForce(Vector2.down * 15f, ForceMode2D.Impulse);
+            if (rb != null) rb.gravityScale = idleGravity;
+            if (rb != null) rb.sharedMaterial = idleMaterial;
+            pounding = false;
+        }
     }
 
     void Move()
     {
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        Debug.Log("Move Input: " + moveInput);
-        if (rb != null) rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, moveInput.x * moveSpeed,0.2f);
+        if (rb != null) rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, moveInput.x * moveSpeed,0.5f);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
